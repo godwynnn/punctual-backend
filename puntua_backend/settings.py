ff1af55f0e2591b8b0474f690868e615e77c4283
godwynnn <miraclegodwin67@gmail.com>
2026-07-03 18:56:08 +0100

    init

--- puntua_backend/settings.py
+++ puntua_backend/settings.py
@@ -251,3 +251,13 @@ RQ_QUEUES = {
        'ASYNC': False,  # Executes tasks instantly in-process for development
      }
  }
+
+# Email Sending Configurations
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'PunctualHr <noreply@punctualhr.com>')
