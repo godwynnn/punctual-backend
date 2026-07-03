acc14f6772721a43708b987092ee699a5d2a0e72
godwynnn <miraclegodwin67@gmail.com>
2026-07-03 19:38:47 +0100

    init

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
-# Exit on error
+# exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-# Convert static asset files
-# python manage.py collectstatic --noinput
-
-# Apply any outstanding database migrations
+# Optional: For Django projects
+python manage.py collectstatic --no-input
 python manage.py migrate
